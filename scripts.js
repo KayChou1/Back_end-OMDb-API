078822072e6af5ca0ff8c33e7e90d0f1c24439e9
unknown <kayajohnson71@yahoo.com>
2020-07-09 15:29:38 -0400

    Added event listeners + function updates

--- scripts.js
+++ scripts.js
@@ -1,32 +1,42 @@
 const container = document.querySelector('.container'); 
-const body = document.querySelector('body'); 
-// const title = document.querySelector('h1'); 
-// const poster = document.querySelector('.poster'); 
-// const rating = document.querySelector('.rating'); 
-// const error = document.querySelector('.error'); 
+const error = document.querySelector('.error'); 
+const submit = document.querySelector(".Submit")
+const title = document.querySelector(".form-control") 
+submit.addEventListener('click',getData);
 
-let getData = () => {
-    fetch('https://www.omdbapi.com/?i=tt3896198&apikey=fb759178')
+function getData(){
+    console.log(title.value)
+    let search = title.value
+    fetch(`https://www.omdbapi.com/?s=${search}&apikey=fb759178`)
         .then(response =>{
             return response.json(); 
         })
         .then(response => {
-            console.log(response); 
-            console.log(response.Search);
             let movies = response.Search 
-            const title = document.createElement('h1'); 
-            container.appendChild(title); 
-            title.innerHTML = movies[0].Title;
-            const poster = document.createElement('img'); 
-            container.appendChild(poster); 
-            poster.setAttribute('src', movies[0].Poster);
+            container.innerHTML ='';
+         createCard(movies)
+
+            
         })
         .catch(err => {
-            console.log(err.Error);
-            error.innerHTML = err.Error; 
+            console.log(err);
+            error.innerHTML = err; 
         })
 };
 
-getData(); 
+function createCard(movies){
+    for (let i = 0; i < movies.length -1; i++) {
 
+        const title = document.createElement('h1'); 
+        const card = document.createElement('div');
+        const poster = document.createElement('img');
+  
+        title.innerHTML = movies[i].Title;
+        card.setAttribute('class',"card")
+        poster.setAttribute('src', movies[i].Poster);
 
+        card.appendChild(title)
+        card.appendChild(poster)
+        container.appendChild(card)
+    }
+}
